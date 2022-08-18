590e35893520795bebc2c3777464375868f60aea
OtienohOwinoh <coowinoh@gmail.com>
2022-08-18 18:26:32 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const hqLocation = 42;
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs (hqLocation - pickupLocation);
+
+
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+
+
+}
+
+function distanceTravelledInFeet (startBlock, destinationBlock) {
+
+    return Math.abs ((startBlock-destinationBlock)*264)
+
+}
+
+function calculatesFarePrice (start, destination) {
+    let distanceTraveled = distanceTravelledInFeet (start, destination) 
+
+    let billableDistance = distanceTraveled - 400;
+
+    if (distanceTraveled <= 400) {
+        return 0
+    }
+
+    else if (distanceTraveled > 400 && distanceTraveled <= 2000){
+        return billableDistance * 2  / 100;
+    }
+
+    else if (distanceTraveled > 2000 && distanceTraveled <=2500){
+        return 25
+    }
+
+    else return  'cannot travel that far'
+    
+}
+
